49db7ce6c8445199173fe9034b5c4bed98136ef3
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-05-01 23:25:12 +0530

    Doubly Circular Linked List

--- DSA/circular_linked_list.c
+++ DSA/circular_linked_list.c
@@ -8,47 +8,210 @@ struct Node
     struct Node *prev;
 };
 struct Node *HEAD, *TAIL = NULL;
+int data, afterwhich;
+void menu();
+void insert_at_beg(int data);
+void insert_in_middle(int afterwhich, int data);
+void insert_at_end(int data);
+void delete_from_beg();
+void delete_from_middle(int afterwhich);
+void delete_from_end();
+void traverse();
 
 int main()
 {
-    struct Node *n1 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *n2 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *n3 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *n4 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *n5 = (struct Node *)malloc(sizeof(struct Node));
+    int option;
+    while (option != 4)
+    {
+        menu();
+        printf("Enter your option : ");
+        scanf("%d", &option);
+        printf("\n");
 
-    struct Node *temp1 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *temp2 = (struct Node *)malloc(sizeof(struct Node));
+        switch (option)
+        {
+        case 1:
+            printf("Enter Data : ");
+            scanf("%d", &data);
+            printf("\n");
+            insert_at_beg(data);
 
-    n1->data = 10;
-    n1->next = n2;
-    n1->prev = TAIL;
-    HEAD = n1;
+        case 2:
+            printf("Enter Data : ");
+            scanf("%d", &data);
+            printf("\n");
+            printf("Enter afterwhich node data do you want to insert : ");
+            scanf("%d", &afterwhich);
+            printf("\n");
+            insert_in_middle(afterwhich, data);
 
-    n2->data = 20;
-    n2->next = n3;
-    n2->prev = n1;
+        case 3:
+            printf("Enter Data : ");
+            scanf("%d", &data);
+            printf("\n");
+            insert_at_end(data);
 
-    n3->data = 30;
-    n3->next = n4;
-    n3->prev = n2;
+        case 4:
+            delete_from_beg();
 
-    n4->data = 40;
-    n4->next = n5;
-    n4->prev = n3;
+        case 5:
+            printf("Enter afterwhich node data do you want to delete : ");
+            scanf("%d", &afterwhich);
+            printf("\n");
+            delete_from_middle(afterwhich);
 
-    n5->data = 50;
-    n5->next = HEAD;
-    n5->prev = n4;
-    TAIL = n5;
+        case 6:
+            delete_from_end();
 
-    temp1 = HEAD;
+        case 7:
+            traverse();
+
+        case 8:
+            printf("Exiting.. ");
+            exit(1);
+        }
+    }
+}
+
+void menu()
+{
+    printf("Choose an option\n");
+    printf("1. Insert at the Beginning\n");
+    printf("2. Insert in the Middle\n");
+    printf("3. Insert at the End\n");
+    printf("4. Delete from the Beginning\n");
+    printf("5. Delete from the Middle\n");
+    printf("6. Delete from the End\n");
+    printf("7. Display the whole circular linked list\n");
+    printf("8. Exit\n");
+}
+
+void insert_at_beg(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+
+    // linked list is empty
+    // this is the first node to be created
+    if (HEAD == NULL && TAIL == NULL)
+    {
+        HEAD = newNode;
+        newNode->next = HEAD;
+        newNode->prev = HEAD;
+        TAIL = newNode;
+    }
+    // there is a linked list with finite number of nodes
+    // newNode to be inserted at the start
+    else if (HEAD != NULL && TAIL != NULL)
+    {
+        newNode->next = HEAD;
+        HEAD->prev = newNode;
+        HEAD = newNode;
+        newNode->prev = TAIL;
+        TAIL->next = newNode;
+    }
+}
+
+void insert_in_middle(int afterwhich, int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *temp, *temp2;
+    temp = HEAD;
+    newNode->data = data;
+
     do
     {
-        printf("%d\n", temp1->data);
-        temp1 = temp1->next;
+        if (temp->data == afterwhich)
+        {
+            temp2 = temp->next;
+            temp->next = newNode;
+            newNode->prev = temp;
+            newNode->next = temp2;
+            temp2->prev = newNode;
+            break;
+        }
+    } while (temp->next != HEAD);
+}
 
-    } while (temp1 != HEAD);
+void insert_at_end(int data)
+{
+    if (HEAD != NULL && TAIL != NULL)
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+        newNode->data = data;
+        struct Node *temp = TAIL;
+        TAIL->next = newNode;
+        newNode->prev = TAIL;
+        newNode->next = HEAD;
+        HEAD->prev = newNode;
+        TAIL = newNode;
+    }
+    else
+    {
+        insert_at_beg(data);
+    }
+}
 
+void delete_from_beg()
+{
+    struct Node *temp, *temp2;
+    temp = HEAD;
+    temp2 = HEAD->next;
+    HEAD = temp2;
+    temp2->prev = TAIL;
+    TAIL->next = temp2;
+    printf("Deleted : %d\n", temp->data);
+    free(temp);
+}
 
+void delete_from_middle(int afterwhich)
+{
+    struct Node *temp, *temp2, *x;
+    temp = HEAD;
+    do
+    {
+        if (temp->data == afterwhich)
+        {
+            x = temp->next;
+            temp2 = temp->next->next;
+            temp->next = temp2;
+            temp2->prev = temp;
+            printf("Deleted : %d\n", x->data);
+            free(x);
+            break;
+        }
+    } while (temp->next != HEAD);
+}
+
+void delete_from_end()
+{
+    struct Node *temp, *temp2, *x;
+    temp = HEAD;
+    do
+    {
+        temp2 = temp->next;
+        if (temp2->next == HEAD)
+        {
+            x = temp->next;
+            temp->next = HEAD;
+            HEAD->prev = temp;
+            TAIL = temp;
+            printf("Deleted : %d\n", x->data);
+            free(x);
+            break;
+        }
+    } while (temp->next != HEAD);
+}
+
+void traverse()
+{
+    struct Node *temp;
+    temp = HEAD;
+    printf("Linked List : ");
+    do
+    {
+        printf("%d->", temp->data);
+        temp = temp->next;
+    } while (temp != HEAD);
+    printf("%d\n", HEAD->data);
 }
